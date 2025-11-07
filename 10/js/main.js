47261b0ff4c04e39ed4b15df705e9b95ef360e84
Keks <keks@htmlacademy.ru>
2025-11-07 18:02:01 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/main.js
+++ 10/js/main.js
@@ -2,15 +2,20 @@ import { similarPhotos } from './creation-photos.js';
 import { renderPhotos } from './render.js';
 import { openBigPhoto } from './full-size-photo.js';
 
-const picturesContainer = document.querySelector('.pictures');
-const thumbnails = picturesContainer.getElementsByClassName('picture');
+const picturesElement = document.querySelector('.pictures');
+const thumbnails = picturesElement.getElementsByClassName('picture');
 
 const photos = similarPhotos();
 renderPhotos(photos);
 
 for (let i = 0; i < thumbnails.length; i++) {
-  thumbnails[i].addEventListener('click', () => {
-    openBigPhoto(photos[i]);
+  thumbnails[i].addEventListener('click', (event) => {
+    event.preventDefault();
+    const currentId = event.target.closest('.picture').id;
+    const currentPhoto = photos.find(
+      (item) => String(item.id) === String(currentId)
+    );
+    openBigPhoto(currentPhoto);
   });
 }
 
