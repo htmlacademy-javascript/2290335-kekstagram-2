9ef9ae7a97dad9ade3931b95713c98a68e4ed27c
Rakhim <pharmamystery19@gmail.com>
2025-11-27 14:36:14 +0700

    Перламутровые пуговицы

--- js/utils.js
+++ js/utils.js
@@ -41,4 +41,22 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-export { getRandomInteger, getRandomArrayElement, isEscapeKey, numDecline, showAlert };
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+export { getRandomInteger, getRandomArrayElement, isEscapeKey, numDecline, showAlert, debounce };
